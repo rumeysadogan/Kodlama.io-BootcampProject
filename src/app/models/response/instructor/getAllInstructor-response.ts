23434f38dbd163314d4b68ee48e2c05215de83cf
Rümeysa Çakır <35909674+rumeysadogan@users.noreply.github.com>
2022-12-06 15:01:12 +0300

    update

--- src/app/models/response/instructor/getAllInstructor-response.ts
+++ src/app/models/response/instructor/getAllInstructor-response.ts
@@ -7,4 +7,10 @@ export interface IGetAllInstructorResponseModel {
   companyName: string;
   nationalIdentity: number;
   dateOfBirth: string;
+  image:string;
+  title:string;
+  instagram:string;
+  twitter:string;
+  github:string;
+
 }
