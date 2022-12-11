5cc5917610e3d8da8f88eb0431374a9d2258f091
mertsubasi <mertsubasi44@gmail.com>
2022-12-11 17:11:40 +0300

    .

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -53,7 +53,6 @@ export class RegisterComponent implements OnInit {
         setTimeout(() => {
           this.router.navigate(['/login']);
         }, 200);
-        0;
       });
     } else {
       this.scroolToTop();
