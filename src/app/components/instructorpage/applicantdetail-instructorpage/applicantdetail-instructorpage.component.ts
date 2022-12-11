df905b47c4029a3427e2e255193a291b1d4d1bf9
elifsenaerdogan <elifsenaerdogan18@gmail.com>
2022-12-11 22:45:06 +0300

    fill applicantdetail component

--- src/app/components/instructorpage/applicantdetail-instructorpage/applicantdetail-instructorpage.component.ts
+++ src/app/components/instructorpage/applicantdetail-instructorpage/applicantdetail-instructorpage.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute } from '@angular/router';
+import { ApplicantService } from 'src/app/services/applicant/applicant.service';
+import { IGetApplicantResponseModel } from './../../../models/response/applicant/getApplicant-response';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicantdetailInstructorpageComponent implements OnInit {
 
-  constructor() { }
+  applicantget:IGetApplicantResponseModel;
+  constructor(private applicantService:ApplicantService,private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getApplicantById();
+  }
+
+  getApplicantById(){
+    this.activatedRoute.params.subscribe((params)=>this.getApplicant(params['id']));
+  }
+
+
+  getApplicant(id){
+    this.applicantService.getApplicantById(id).subscribe((data)=>{
+      this.applicantget=data;
+    });
+    
+
   }
 
 }
